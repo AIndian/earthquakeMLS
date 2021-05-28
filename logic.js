41c158288225fc7a756081903b9951a3d82352ce
Amol Bhatia <amol.k.bhatia@gmail.com>
2021-05-27 20:56:41 -0400

    updated files

--- logic.js
+++ logic.js
@@ -4,87 +4,95 @@ var earthquakeMagnitude = 4
 var citystatezip = 19104
 var address = "Pheonix, AZ"
 var houseURL =  `http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=${ZWSID}&address=${address}&citystatezip=${citystatezip}`;
-var faultURL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&endtime=2021-05-01"+
+var faultURL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2021-05-01"+
 `&maxlongitude=${Region[1][1]}&minlongitude=${Region[0][1]}&maxlatitude=${Region[0][0]}&minlatitude=${Region[1][0]}&minmagnitude=${earthquakeMagnitude}`;
 
 var lng = -117.4313333
 var lat = 35.5303333
 var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${YOUR_API_KEY}&location_type=ROOFTOP`
 
-var queryURL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&endtime=" +
-  "2021-05-02&maxlongitude=-105.61&minlongitude=-124.49&maxlatitude=37.81&minlatitude=32.06&minmagnitude=4";
-
 var zipcodeURL = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=&rows=3000&facet=state&facet=timezone&facet=dst&geofilter.polygon=(37.81%2C+-124.49)%2C+(37.81%2C+-105.61)%2C+(32.06%2C+-105.61)%2C+(32.06%2C-124.49)"
 
+  var myMap = L.map("map", {
+    center: [
+      35.0, -115.71
+    ],
+    zoom: 7,
+  });
 
-d3.json(faultURL).then(function(EQ) {
-    createFeatures(EQ.features);
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
 
 
-    });
-
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
+d3.json(faultURL).then(function(earthquakeData) {
+d3.json()
+    function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place + "coordinates: " + feature.geometry.coordinates +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-
-  // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 10,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  });
-
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        });
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 10,
     id: "dark-v10",
     accessToken: API_KEY
-  });
+        });
 
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
 
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      35.0, -115.71
-    ],
-    zoom: 7,
-    layers: [streetmap, earthquakes]
-  });
+    console.log(earthquakeData);
+    // var magnitude = []
+    var lat_long = [];
+    var i;
+    for (i = 0; i < earthquakeData.features.length; i ++){
+
+        var geometry = earthquakeData.features[i].geometry;
+        var properties = earthquakeData.features[i].properties;
+        if (geometry) {
+          lat_long.push([geometry.coordinates[1], geometry.coordinates[0], properties.mag]);
+        }
+    }
+
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-}
+//  L.control.layers(baseMaps, overlayMaps, {
+//    collapsed: false
+//    })
+
+    var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+     };
+
+    var heat = L.heatLayer(lat_long, {
+    radius: 15,
+    maxZoom: 5
+    }).addTo(myMap)
+
+    var overlayMaps = {
+    Earthquakes: heat,
+    Houses: heat
+    };
+
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
+
+});
+
+
